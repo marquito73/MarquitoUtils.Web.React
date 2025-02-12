74222425938402d554a38315ac8cc4b7dd086c83
theo begyn <theobegyn@live.com>
2025-02-12 19:39:44 +0100

    Adding file service for web

---
+++ MarquitoUtils.Web.React/Class/Service/IWebFileService.cs
@@ -0,0 +1,16 @@
+﻿using MarquitoUtils.Main.Class.Entities.File;
+using MarquitoUtils.Main.Class.Service.Files;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarquitoUtils.Web.React.Class.Service
+{
+    public interface IWebFileService : IFileService
+    {
+        public CustomFile GetFileStreamFromWebFile(IFormFile formFile, string fileExtension);
+    }
+}
---
+++ MarquitoUtils.Web.React/Class/Service/WebFileService.cs
@@ -0,0 +1,36 @@
+﻿using MarquitoUtils.Main.Class.Entities.File;
+using MarquitoUtils.Main.Class.Service.Files;
+using MarquitoUtils.Main.Class.Tools;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarquitoUtils.Web.React.Class.Service
+{
+    public class WebFileService : FileService, IWebFileService
+    {
+        public CustomFile GetFileStreamFromWebFile(IFormFile formFile, string fileExtension)
+        {
+            CustomFile file;
+
+            if (this.TextExtensions.Contains(fileExtension))
+            {
+                // We can read file as text
+                using (StreamReader reader = new StreamReader(formFile.OpenReadStream()))
+                {
+                    file = new CustomFile(formFile.Name, fileExtension, reader.ReadToEnd());
+                }
+            }
+            else
+            {
+                file = new CustomFile(formFile.Name, fileExtension, Utils.ReadAllBytes(formFile.OpenReadStream()));
+            }
+
+            return file;
+        }
+    }
+}
