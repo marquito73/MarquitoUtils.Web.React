c4685b45c3bbc4f0378931d186277e9354ee0def
theo begyn <theobegyn@live.com>
2025-04-10 11:18:50 +0200

    Adding side bar component

---
+++ MarquitoUtils.Web.React/Class/SideBar/MenuEntry.cs
@@ -0,0 +1,33 @@
+﻿namespace MarquitoUtils.Web.React.Class.SideBar
+{
+    /// <summary>
+    /// A menu entry for the side bar
+    /// </summary>
+    public sealed class MenuEntry
+    {
+        /// <summary>
+        /// The entry key
+        /// </summary>
+        public string EntryKey { get; set; }
+        /// <summary>
+        /// An icon to display in the entry's header
+        /// </summary>
+        public string IconClass { get; set; }
+        /// <summary>
+        /// A label to display in the entry's header
+        /// </summary>
+        public string Label { get; set; }
+        /// <summary>
+        /// The entry is selected ?
+        /// </summary>
+        public bool Selected { get; set; }
+        /// <summary>
+        /// The view URL to load
+        /// </summary>
+        public string ViewURL { get; set; }
+        /// <summary>
+        /// Sub entries for this entry
+        /// </summary>
+        public List<MenuEntry> SubEntries { get; set; } = new List<MenuEntry>();
+    }
+}
---
+++ MarquitoUtils.Web.React/Class/SideBar/SideBar.cs
@@ -0,0 +1,52 @@
+﻿using MarquitoUtils.Web.React.Class.Components;
+
+namespace MarquitoUtils.Web.React.Class.SideBar
+{
+    /// <summary>
+    /// A side bar component, display a list of menu entries, for load view or execute actions
+    /// </summary>
+    public class SideBar : Component
+    {
+        public string Title { get; set; }
+        /// <summary>
+        /// Entries and sub entries of the side bar
+        /// </summary>
+        public List<MenuEntry> Entries { get; set; }
+        /// <summary>
+        /// The width of the side bar
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// The color for the background
+        /// </summary>
+        public string BackgroundColor { get; set; }
+        /// <summary>
+        /// The color for entries and sub entries
+        /// </summary>
+        public string EntryColor { get; set; }
+        /// <summary>
+        /// The color for selected entries and sub entries
+        /// </summary>
+        public string EntrySelectedColor { get; set; }
+        /// <summary>
+        /// The color for hovered entries and sub entries
+        /// </summary>
+        public string EntryHoverColor { get; set; }
+        /// <summary>
+        /// The color that outline the side bar
+        /// </summary>
+        public string OutlineColor { get; set; }
+        /// <summary>
+        /// The view container, to load when user click on a menu entry
+        /// </summary>
+        public string ViewContainerID { get; set; }
+
+        /// <summary>
+        /// A side bar component, display a list of menu entries, for load view or execute actions
+        /// </summary>
+        /// <param name="id">His ID</param>
+        public SideBar(string id) : base(id)
+        {
+        }
+    }
+}
